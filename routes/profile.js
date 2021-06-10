97d598ae66c671898686bb1eaa700f190775f41e
Keshav Agarwal <keshavagarwal1710@gmail.com>
2021-06-10 19:51:18 +0530

    added mail functionality and added batch in backend

--- routes/profile.js
+++ routes/profile.js
@@ -1,11 +1,66 @@
 const express = require("express");
 const router = express.Router();
-const {authRequired} = require("../middleware/auth")
+const { authRequired } = require("../middleware/auth")
+var firebase = require("firebase/app")
+const app = require('../fire');
+const db = firebase.firestore(app);
+const nodemailer = require('nodemailer')
 
-router.get("/",authRequired,(req,res)=>{
+router.get("/", authRequired, (req, res) => {
     const token = req.token;
-    console.log(token);
-    res.send("HELLO")
+    var docRef = db.collection("users").doc(token);
+    docRef.get().then((doc) => {
+        if (doc.exists) {
+            res.send(doc.data());
+        } else {
+            // doc.data() will be undefined in this case
+            res.status(401).send("User Not Found");
+        }
+    }).catch((error) => {
+        console.log("Error getting document:", error);
+        res.status(500).send("Server Error");
+    });
+})
+
+router.post("/setbatch", authRequired, (req, res) => {
+    try {
+        const { gmail, batchno } = req.body;
+        const token = req.token;
+        if (gmail && gmail.trim() !== '') {
+            const link = `http://localhost:3000/formRegister/${token}/${batchno}`;
+            var transporter = nodemailer.createTransport({
+                service: 'gmail',
+                auth: {
+                    user: process.env.email,
+                    pass: process.env.password
+                }
+            });
+
+            var mailOptions = {
+                from: process.env.email,
+                to: gmail,
+                subject: `Please Fill The Form To Join Batch ${batchno}`,
+                html: `<p>Hello</p> <p>Click on below link to fill the form to join batch ${batchno}</p> <button style="background-color:#0a1d37;padding:13px; border:none;"><a href=${link} style="font-size:22px;color:white;text-decoration:none;">Click Here</a></button>`
+            };
+
+            transporter.sendMail(mailOptions, function (error, info) {
+                if (error) {
+                    console.log(error.message);
+                } else {
+                    console.log('Email sent: ' + info.response);
+                }
+            });
+        }
+        db.collection('users').doc(token).collection("batch").doc(batchno).set({
+            name: batchno,
+            students: []
+        })
+
+        res.send({ token,success: true });
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send({ error: error.message })
+    }
 })
 
 module.exports = router;
