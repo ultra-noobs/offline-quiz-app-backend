502496c43c5eca43d97990f4380298f454ff0aa7
nlok5923 <nlokhande5923@gmail.com>
2021-06-12 15:31:13 +0530

    enabled teacher to select batch

--- routes/dashboardRouter.js
+++ routes/dashboardRouter.js
@@ -8,9 +8,10 @@ const { questionFilter } = require('../utils/questionFilter')
 
 router.post("/saveQuiz",authRequired,(req,res)=> {
     const data = req.body.finalQnA;
-    const { time, date, title } = req.body;
+    const { time, date, title, batch } = req.body;
+    console.log(batch);
     const finalQuizArray = questionFilter(data);
-    db.collection('users').doc(req.token).collection("quiz").add( { time, date, title, finalQuizArray});
+    db.collection('users').doc(req.token).collection("quiz").add( { time, date, title, batch, finalQuizArray});
     console.log("Quiz Successfully Created");
     res.send("Quiz Successfully Created")
 })
@@ -36,6 +37,16 @@ router.post("/saveQuiz",authRequired,(req,res)=> {
         console.log(querySnapshot.data());
         res.send(querySnapshot.data());
     });
-});
+})
+
+.get('/quizbatches', authRequired, async (req, res) => {
+    const quizDocRef = db.collection('users').doc(req.token).collection('batch');
+    let batches = [];
+    await quizDocRef.get().then((querySnapshot) => {
+       querySnapshot.forEach((doc, index) => batches.push({value: doc.data().name ,  text: doc.data().name }));
+    })
+    console.log(batches);
+    res.send(batches);
+})
 
 module.exports = router;
