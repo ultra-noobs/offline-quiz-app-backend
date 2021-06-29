6fec20bae23e9bfb7124634f134ee7de933aaf2e
nlok5923 <nlokhande5923@gmail.com>
2021-06-29 14:23:30 +0530

    rm redundant comments

--- app.js
+++ app.js
@@ -42,22 +42,6 @@ app.use("/register", registerRouter);
 app.use("/dashboard/", dashboardRoute);
 app.use("/profile", profileRoute);
 
-// catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
-
-// error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
-
 app.listen(port, () => {
   console.log("listening to port", port);
 });
